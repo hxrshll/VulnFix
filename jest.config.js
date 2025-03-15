33464958a066ad82b96e4c5c9be769c515150ad5
Harshal Khadse <hkharshal28@gmail.com>
2025-03-15 19:37:10 +0530

    monor update

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    transform: {
+      "^.+\\.jsx?$": "babel-jest",
+    },
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  };
+  
